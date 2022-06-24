058bb035b24cc502a94dc426cafd0f83f55f2ceb
Saint-Pierre KOUADIO <50495968+spkouadio@users.noreply.github.com>
2022-06-24 21:38:34 +0000

    Update droplet_descr.py

--- script/droplet_descr.py
+++ script/droplet_descr.py
@@ -2,6 +2,8 @@
 
 import const as cst
 import math
+import numpy as np
+import matplotlib.pyplot as plt
 
 humidity = 0.5 #50%
 temp = 30 #°C
@@ -19,7 +21,7 @@ def sed_velocity(drop_diam):
     r'''The sedimentation velocity of the droplet is obtained at equilibrium (dV/dt=0),
     under conditions close to the ground where the wind movement is horizontal
         *:param drop_diam: droplet diameter depend of weigth fraction
-        *:return: sedimentation velocity of différente droplet
+        *:return: sedimentation velocity of differente droplet
     '''
     rho_vol = cst.rho_mix_vol(chem_dilrate, chem_density, supp_density)
     rho_a = cst.rho_a(humidity, temp)
@@ -30,8 +32,28 @@ def sed_velocity(drop_diam):
 
 #
 def drop_distrib(mixt_volume):
-    r'''Droplet distribution based on the Stochastic Rosin-Rammler Diameter Distribution Method
-        *:param mixt_volume:
-        *:return:
+    r'''
+    Droplet distribution based on the log-normal distribution function
+    :param mixt_volume:
+    :return:
     '''
-    return
+    n0 = 0.567
+    sigma_g = 1.3
+    d_50 = 254.74
+    d_inf = 40
+    d_sup = 500
+    f_cumul = 0
+    drop_table = np.empty((0, 3), float)
+    for d in np.arange(d_inf, d_sup, sigma_g):
+        f = (n0/(math.sqrt(2*math.pi)*d*math.log10(sigma_g)))\
+            *math.exp(-math.pow(math.log10(d/d_50),2)/(2*math.pow(math.log10(sigma_g),2)))
+        f_cumul = f_cumul + f
+        drop_table = np.append(drop_table, np.array([[d, f, f_cumul]]), axis=0)
+    return drop_table
+
+# Plotting point using scatter method
+X= drop_distrib(0)[:, 0]
+Y1= drop_distrib(0)[:, 1]
+Y2= drop_distrib(0)[:, 2]
+plt.scatter(X,Y1 )
+plt.show()
